996f6f751c90126e941734e99c8b209f44dc2525
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2025-12-27 15:02:53 +0200

    Data types  [value & reference]

---
+++ Demo1.CSharp/Point.cs
@@ -0,0 +1,8 @@
+﻿namespace Demo1.CSharp
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+    }
+}
--- Demo1.CSharp/Program.cs
+++ Demo1.CSharp/Program.cs
@@ -37,6 +37,61 @@
             // CTS : Contains all languages, data types and CLS (Container of .Net system) 
             #endregion
 
+            #region Value type-stack
+            //// Value type - Stack
+            //int number; // Allocate  UnInitialzed 4 bytes in stack.
+
+            ////Console.WriteLine(number); // Invalid (Unassigned variable)
+
+            //number = 10;
+
+            //Console.WriteLine(number); // valid(Assigned variable)
+
+            //// int : C# Keyword
+
+            //// int32 : BCL Keyword
+
+            //Int32 number2 = 12;
+
+            //Console.WriteLine(number2);
+
+            //// The most common used is C# keyWords 
+            #endregion
+
+            #region Reference type-heap
+            //Point point;
+            //// Declare to reference from data type point in stack
+            //// Can refer to instance from type Point in Heap.
+            //// P1 : refer to null.
+            //// 8 bytes will be allocated in stack to the reference
+
+            ////Console.WriteLine(point.X); // Invalid
+
+            //point = new Point();
+            //// new : 
+            //// 1. Allocate the number of required bytes to the instance (point) in heap memory
+            //// 2. Initialized the data type in the instance with the default values.
+            //// 3. Call User_Definded constructor if exist
+            //// 4. Assigned object Address or instance address to point variable in stack
+
+            //Console.WriteLine(point.x); // 0;
+            //Console.WriteLine(point.y); // 0;
+
+            //point.x = 12;
+
+            //Console.WriteLine(point); // 12
+
+            //Point point2 = new Point();
+
+            //Console.WriteLine(point2.x); // 0
+            //Console.WriteLine(point2.y); // 0
+
+            //point2 = point;
+
+            //Console.WriteLine(point2.x); // 12 
+            #endregion
+
+
 
         }
     }
